b1ae52f710b12e0abcd3fd85814535640c530a6d
Vishwamithra37 <53423141+Vishwamithra37@users.noreply.github.com>
2021-07-16 09:49:40 +0530

    Add files via upload

--- version2.py
+++ version2.py
@@ -16,8 +16,10 @@ class a1:
     def treader(me):
         me["auth"]["token"]=flask.request.cookies.get('token')
         me["auth"]["state"]=flask.request.cookies.get('state')
+        me["auth"]["district"]=flask.request.cookies.get('district')
         assert type(me["auth"]["token"])==str and len(me["auth"]["token"])==26
         assert type(me["auth"]["state"])==str 
+        assert type(me["auth"]["district"])==str
         a=flask.Response(json.dumps(me))  
         a.delete_cookie("token") #Key with name token gets deleted
         a.set_cookie('ola','chela') #Ola:chela cookie gets set
@@ -35,9 +37,11 @@ class a1:
         fire=dac.find_one({"auth.token":me["auth"]["token"]},
                           {"_id":1,"user.email":1,
                           "user.election circles":1,
-                           "user.state":1,"user.name":1,"user.party":1})
+                           "user.state":1,"user.name":1,
+                           "user.party":1,"user.district":1})
         assert fire!=None
         assert me["auth"]["state"]==fire["user"]["state"]
+        assert me["auth"]["district"]==fire["user"]["district"]
         assert me["auth"]["election circles"] in fire["user"]["election circles"]
         fire["auth"]["token"]=me["auth"]["token"]                                 ##Use fire to create a token   
         return fire
@@ -153,8 +157,70 @@ class a1:
             return {"Operation":"successful"}
 
 class maker():
-   def questionmaker(me):
+    def questionmaker(me,fire):
+                me1=nd()
+                me1["q"]["m_question"]=me["q"]["m_question"]
+                me1["q"]["d_question"]=me["q"]["d_question"]
+                me1["q"]["tags"]=me["q"]["tags"]
+                me1["q"]["election circles"]=me["auth"]["election circles"]
+                me1["q"]["time_stamp"]=str(round(time.time()))
+                me1["q"]["followers"]=1
+                me1["q"]["author"]=fire["user"]["name"]
+                me1["q"]["state"]=fire["user"]["state"]
+                me1["q"]["nation"]=fire["user"]["nation"]
+                me1["q"]["district"]=fire["user"]["district"]
+                me1["q"]["noofanswers"]=0
+                me1["auth"]["users"]=[fire["user"]["email"],"mithravishwa37@gmail.com"]      #Add the ability to add security staff                                          
+
+                dab=db["feed"]
+                dac=dab[fire["user"]["state"]]               
+                dac.insert_one(me1) 
+
+                me2=nd()
+                me2["q"]["hash"]=str(me1["_id"])
+                me2["q"]["election circles"]=me["auth"]["election circles"]
+                me2["followers"][fire["user"]["email"]]=fire["user"]["email"]
+
+                dab=db["qfollowers"]
+                dac=dab[fire["user"]["state"]]
+                dac.insert_one(me2)
+
+                return str(me1["q"]["hash"])
+
+    def solutionmaker(me,fire):
+
+            dab=db["solutions"]
+            dac=dab[fire["user"]["state"]]
+            dac.find_one({"q.hash":me["q"]["hash"],"main.user":fire["user"]["email"]})==None
+            
+            me1=nd()
+            me1["q"]["hash"]=me["q"]["hash"]
+            me1["s"]["solution"]=me["s"]["solution"]
+            me1["s"]["name"]=fire["user"]["name"]
+            me1["s"]["time_stamp"]=str(round(time.time()))
+            me1["s"]["upvotes"]=0
+            me1["s"]["downvotes"]=0
+            me1["s"]["election circles"]=me["auth"]["election circles"]
+            me1["s"]["state"]=fire["auth"]["state"]
+            me1["s"]["nation"]=fire["auth"]["nation"]
+            me1["s"]["comments"]="no"
+            me1["auth"]["users"]=["mithravishwa37@gmail.com",fire["user"]["email"]]
+            me1["main"]["user"]=fire["user"]["email"]      
+            try:
+                me1["s"]["party"]=fire["user"]["party"]
+            except:
+                pass     
+
+            dac.insert_one(me1)
+         
+            dab=db["feed"]
+            dac=dab[fire["user"]["state"]]
+            dac.update_one({"_id":ObjectId(me["q"]["hash"])},{"$inc":{"q.noofanswers":1},
+                            "$push":{"qs.hashes":str(me1["_id"])}})###
+
         
+             
+          
 
 
 class uad:
